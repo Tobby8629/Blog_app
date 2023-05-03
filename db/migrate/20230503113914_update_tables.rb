77db8ab1be6089040be1172e37f6e9a62135ea05
tobby8629 <sarhmullah15@gmail.com>
2023-05-03 05:41:11 -0700

    update the schema file

---
+++ db/migrate/20230503113914_update_tables.rb
@@ -0,0 +1,10 @@
+class UpdateTables < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :posts, :author_id_id, :author_id
+    rename_column :posts, :commentCounter, :comment_counter
+    rename_column :likes, :author_id_id, :author_id
+    rename_column :likes, :post_id_id, :post_id
+    rename_column :users, :postsCounter, :posts_counter
+    rename_column :posts, :likeCounter, :likes_counter
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_211542) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_113914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,23 +23,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_211542) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "post_id_id", null: false
-    t.bigint "author_id_id", null: false
+    t.bigint "post_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id_id"], name: "index_likes_on_author_id_id"
-    t.index ["post_id_id"], name: "index_likes_on_post_id_id"
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.bigint "author_id_id", null: false
+    t.bigint "author_id", null: false
     t.string "title"
     t.text "text"
-    t.integer "commentCounter"
-    t.integer "likeCounter"
+    t.integer "comment_counter"
+    t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id_id"], name: "index_posts_on_author_id_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,12 +48,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_211542) do
     t.string "name"
     t.text "photo"
     t.string "bio"
-    t.integer "postsCounter"
+    t.integer "posts_counter"
   end
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
-  add_foreign_key "likes", "posts", column: "post_id_id"
-  add_foreign_key "likes", "users", column: "author_id_id"
-  add_foreign_key "posts", "users", column: "author_id_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
