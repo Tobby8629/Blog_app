7c86b5a0f80087b5be6841110d031279d79b6fd1
tobby8629 <sarhmullah15@gmail.com>
2023-05-03 21:49:30 -0700

    clear errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ApplicationRecord
-    has_many :comments, class_name: "Comment", foreign_key: "post_id"
-    has_many :likes, class_name: "Like", foreign_key: "post_id"
-    belongs_to :user, class_name: "User", foreign_key: "author_id"
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
 
-    def posts_counter(user)
-     array = user.posts
-     user.posts_counter = array.length
-    end
+  def posts_counter(user)
+    array = user.posts
+    user.posts_counter = array.length
+  end
 
-    def recent_comment
-     array=self.comments
-     array[-5..-1]
-    end
+  def recent_comment
+    array = comments
+    array[-5..]
+  end
 end
