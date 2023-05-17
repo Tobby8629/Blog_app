6f1adb521090531d86fa3a4e67962176c8ac71e2
Samuel tobby <sarhmullah15@gmail.com>
2023-05-17 14:17:34 -0700

    update pull request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :posts_counter, :email, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :posts_counter, :email, :password, :current_password] )
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[name bio posts_counter email password password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[name bio posts_counter email password current_password])
   end
 end
-
